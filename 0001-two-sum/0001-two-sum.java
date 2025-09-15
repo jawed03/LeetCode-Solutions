4fdd340838000243600062645006f3502ff62791
Md Jawed <jawednaushad@gmail.com>
2025-09-15 13:05:02 +0530

    Time: 2 ms (98.89%), Space: 44.9 MB (64.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,51 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-                         // GENERAL SOLUTION... (N*N, 1)
+                        //  Using HashMap... (N*logN, S.C -> N)
 
-        for(int i=0; i<nums.length-1; i++)
-        {
-            for(int j=i+1; j<nums.length; j++)
-            {
-                if(nums[i] + nums[j] == target)
-                {
-                  return new int []{i,j};
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int []{map.get(target - nums[i]), i};
+            }
+            else{
+                map.put(nums[i], i);
             }
         }
-        return new int[]{};
+        return new int []{};
 
 
+                          // GENERAL SOLUTION... (N*N, 1)
+
+        // for(int i=0; i<nums.length-1; i++)
+        // {
+        //     for(int j=i+1; j<nums.length; j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //         {
+        //           return new int []{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+
+
+
+                        // OPTIMISED SOLUTION (Only for Sorted Array)
+
+        // int low=0, high=nums.length-1;
+        // while(low < high){
+        //     if(nums[low] + nums[high] == target)
+        //     return new int []{low, high};
+        //     else if(nums[low] + nums[high] > target)
+        //     high--;
+        //     else 
+        //     low++;
+        // }
+        // return new int[]{};
+
+        // Arrays.sort(nums);
+        // In this Method, only can track index of sorted Array
+        // Not to Original Array
     }
 }
