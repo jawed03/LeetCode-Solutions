f8d4ff22d412dd1fa117d0f86b8186ddab77cc00
Md Jawed <jawednaushad@gmail.com>
2026-01-20 17:07:27 +0530

    Time: 3 ms (87.98%), Space: 45 MB (88.33%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+                                // OPTIMAL SOLUTION
+
+        int left =0, right =0;
+        int maxLen =0;
+        int n = s.length();
+        int hash[] = new int [256];
+        Arrays.fill(hash, -1);
+
+        while(right < n){
+            // check charater is present in Hash or not.
+            // If present, then make sure it is not between left and right pointer
+            if(hash[s.charAt(right)] != -1){
+                if(hash[s.charAt(right)] >= left){
+                left = hash[s.charAt(right)] +1;
+                }
+            }
+
+            // find maximum length of substring.
+            // Store the current index of charater into Hash table
+            int len = right - left +1;
+            maxLen = Math.max(maxLen, len);
+            hash[s.charAt(right)] = right;
+            right++;
+        }
+
+        return maxLen;
+
+        // T.C --> O(n)
+        // S.C --> O(256)
+
+
+                                    // BRUTE FORCE APPROACH
+
+        // int len =0, maxLen =0;
+        // int n = s.length();
+        // for(int i=0; i<n; i++){
+        // int hash[] = new int[256];      // Hash always start with zero elements
+        //     for(int j=i; j<n; j++){
+        //         // If charater already visited, terminate the 2nd loop
+        //         if(hash[s.charAt(j)] != 0){
+        //             break;
+        //         }
+        //         len = j -i +1;
+        //         // Mark the table as charater is visited
+        //         hash[s.charAt(j)]++;
+        //     }
+        //     maxLen = Math.max(maxLen, len);
+        // }
+        // return maxLen;
+
+        // T.C --> O(n*n)
+        // S.C --> O(256)
+    }
+}
