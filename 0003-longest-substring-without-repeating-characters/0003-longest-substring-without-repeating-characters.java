61dc5e7c895c6cef5d503d14cf3e120e9a2e9136
Md Jawed <jawednaushad@gmail.com>
2026-01-20 20:05:24 +0530

    Time: 5 ms (85.29%), Space: 45.6 MB (73.2%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,30 +3,47 @@ class Solution {
 
                                 // OPTIMAL SOLUTION
 
-        int left =0, right =0;
-        int maxLen =0;
-        int n = s.length();
-        int hash[] = new int [256];
-        Arrays.fill(hash, -1);
+        // USING HASHMAP
+        Map<Character, Integer> map = new HashMap<>();
+        int left, right, maxLen;
+        left = right = maxLen = 0;
 
-        while(right < n){
-            // check charater is present in Hash or not.
-            // If present, then make sure it is not between left and right pointer
-            if(hash[s.charAt(right)] != -1){
-                if(hash[s.charAt(right)] >= left){
-                left = hash[s.charAt(right)] +1;
-                }
+        while(right < s.length()){
+            char ch = s.charAt(right);
+            if(map.containsKey(ch) &&  left <= map.get(ch)){
+                left = map.get(ch) + 1;
             }
-
-            // find maximum length of substring.
-            // Store the current index of charater into Hash table
-            int len = right - left +1;
-            maxLen = Math.max(maxLen, len);
-            hash[s.charAt(right)] = right;
+            map.put(ch, right);
+            maxLen = Math.max(maxLen, right - left + 1);
             right++;
         }
-
         return maxLen;
+
+
+        // int left =0, right =0;
+        // int maxLen =0;
+        // int n = s.length();
+        // int hash[] = new int [256];
+        // Arrays.fill(hash, -1);
+
+        // while(right < n){
+        //     // check charater is present in Hash or not.
+        //     // If present, then make sure it is not between left and right pointer
+        //     if(hash[s.charAt(right)] != -1){
+        //         if(hash[s.charAt(right)] >= left){
+        //         left = hash[s.charAt(right)] +1;
+        //         }
+        //     }
+
+        //     // find maximum length of substring.
+        //     // Store the current index of charater into Hash table
+        //     int len = right - left +1;
+        //     maxLen = Math.max(maxLen, len);
+        //     hash[s.charAt(right)] = right;
+        //     right++;
+        // }
+
+        // return maxLen;
 
         // T.C --> O(n)
         // S.C --> O(256)
