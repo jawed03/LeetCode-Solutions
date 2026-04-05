7393b9adc597d158e573b3f908a6f2c5a35a687e
Md Jawed <jawednaushad@gmail.com>
2026-04-05 16:46:11 +0530

    Time: 0 ms (100%), Space: 44 MB (95.59%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,44 +11,51 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
     
-        ListNode ans = new ListNode();
-        ListNode ansHead = ans;
+    // Create new node(temp) to link all node into sorted order
+        ListNode temp = new ListNode();
+    // To hold reference of temp's head
+        ListNode tempHead = temp;
 
         ListNode curr1 = list1;
         ListNode curr2 = list2;
 
+    // For null node
         if(curr1 == null)
         return curr2;
-
         if(curr2 == null)
         return curr1;
 
+    // Iterate till reach end of any list
         while(curr1 != null && curr2 != null){
             if(curr1.val <= curr2.val){
-                ansHead.next = curr1;
-                ansHead = ansHead.next;
+                temp.next = curr1;
+                temp = temp.next;
                 curr1 = curr1.next;
-            } else {
-                ansHead.next = curr2;
-                ansHead = ansHead.next;
+            }
+            else{
+                temp.next = curr2;
+                temp = temp.next;
                 curr2 = curr2.next;
             }
         }
 
+    // Link nodes of list1 if any node remain
         while(curr1 != null){
-            ansHead.next = curr1;
-            ansHead = ansHead.next;
+            temp.next = curr1;
+            temp = temp.next;
             curr1 = curr1.next;
         }
+
+    // Link nodes of list2 if any node remain
         while(curr2 != null){
-            ansHead.next = curr2;
-            ansHead = ansHead.next;
+            temp.next = curr2;
+            temp = temp.next;
             curr2 = curr2.next;
         }
-        return ans.next;
+        return tempHead.next;
 
-        // Brute Force Method
 
+                // Brute Force Method
         // 1. store all the elements of both LL into a ArrayList.
         // 2. Sort it by using collection.sort.
         // 3. then create LL from sorted array.
