d922a5cf67baf5c7acb76703cefa0d00df5dc48c
Md Jawed <jawednaushad@gmail.com>
2026-04-07 17:21:51 +0530

    Time: 26 ms (25.83%), Space: 76 MB (88.19%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,26 +3,39 @@ class Solution {
         if(k == 0)
         return false;
 
-        HashSet<Integer> seenNumbers = new HashSet<>();
-        int right;
-        int left = 0;
-
-        // Create K length window.
-        for(right=0; right <= k && right < nums.length; right++){
-            if(seenNumbers.contains(nums[right]))
-            return true;
-            seenNumbers.add(nums[right]);
-        }
-
-        // Check duplicate.
-        while(right < nums.length) {
-            seenNumbers.remove(nums[left]);
-            left++;
-            if(seenNumbers.contains(nums[right]))
-            return true;
-            seenNumbers.add(nums[right]);
-            right++;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i - k]);
+            }
         }
         return false;
+
+                // Same method but other way.. 
+
+        // HashSet<Integer> seenNumbers = new HashSet<>();
+        // int right;
+        // int left = 0;
+
+        // // Create K length window.
+        // for(right=0; right <= k && right < nums.length; right++){
+        //     if(seenNumbers.contains(nums[right]))
+        //     return true;
+        //     seenNumbers.add(nums[right]);
+        // }
+
+        // // Check duplicate.
+        // while(right < nums.length) {
+        //     seenNumbers.remove(nums[left]);
+        //     left++;
+        //     if(seenNumbers.contains(nums[right]))
+        //     return true;
+        //     seenNumbers.add(nums[right]);
+        //     right++;
+        // }
+        // return false;
     }
 }
