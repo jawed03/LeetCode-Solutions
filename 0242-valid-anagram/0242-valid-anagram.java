e8413ffe4dcc2fcf8b4c5de36472be68cadea29b
Md Jawed <jawednaushad@gmail.com>
2026-04-07 21:16:16 +0530

    Time: 6 ms (43.44%), Space: 44.2 MB (92.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        // Both strings should be equal length.
+
         if(s.length() != t.length())
         return false;
 
-        int[] charCounts = new int[26];     // Assuming only lowercase English letters
+        char [] hash = new char[26];
 
-        // Increment count for each character in 's' and Decrement for each in 't'.
         for(int i=0; i<s.length(); i++){
-            charCounts[s.charAt(i) - 'a']++;
-            charCounts[t.charAt(i) - 'a']--;
+            hash[s.charAt(i) - 'a']++;
+            hash[t.charAt(i) - 'a']--;
         }
-
-        // Chech if all counts are zero
-        for(int count : charCounts){
-            if(count != 0){
+        for(int ele : hash){
+            if(ele != 0)
                 return false;
-            }
         }
-
         return true;
+        
+        // // Both strings should be equal length.
+        // if(s.length() != t.length())
+        // return false;
+
+        // int[] charCounts = new int[26];     // Assuming only lowercase English letters
+
+        // // Increment count for each character in 's' and Decrement for each in 't'.
+        // for(int i=0; i<s.length(); i++){
+        //     charCounts[s.charAt(i) - 'a']++;
+        //     charCounts[t.charAt(i) - 'a']--;
+        // }
+
+        // // Chech if all counts are zero
+        // for(int count : charCounts){
+        //     if(count != 0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
     }
 }
