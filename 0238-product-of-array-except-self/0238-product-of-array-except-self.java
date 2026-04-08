541603818395b5950c2de9a49ca11ab586643fb3
Md Jawed <jawednaushad@gmail.com>
2026-04-08 20:14:54 +0530

    Time: 2 ms (95.06%), Space: 63.3 MB (98.49%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        // T.C -> O(N + N)
+        // S.C -> O(N + N)
+
+        int n = nums.length;
+        int prefixProduct [] = new int [n];
+        int postfixProduct [] = new int [n];
+        int prefix = 1;
+        int postfix = 1;
+
+        // find prefix product of given Array...
+        for(int i=0; i<n; i++){
+            prefixProduct[i] = prefix;
+            prefix *= nums[i];
+        }
+        // find postfix product of given Array...
+        for(int i=n-1; i>=0; i--){
+            postfixProduct[i] = postfix;
+            postfix *= nums[i];
+        }
+
+        // Multiply prefix and postfix to get answer.
+        for(int i=0; i<n; i++){
+            nums[i] = prefixProduct[i]*postfixProduct[i];
+        }
+        return nums;
+
+        
+
+        //Reduce S.C -> O(N)
+        //T.C -> O(N + N)
+
+        // int n = nums.length;
+        // int ans [] = new int [n];
+        // int curr = 1;
+
+        // for(int i=0; i<n; i++){
+        //     ans[i] = curr;
+        //     curr *= nums[i];
+        // } 
+        // curr = 1;
+        // for(int i=n-1; i>=0; i--){
+        //     ans[i] *= curr;
+        //     curr *= nums[i];
+        // }
+        // return ans;
+
+
+
+            // BRUTE FORCE METHOD [TC -> O(n*n) && SC -> O(1)]
+        
+        // for(int i = 0; i < n; i++) {
+        //     int pro = 1;
+        //     for(int j = 0; j < n; j++) {
+        //         if(i == j) continue;
+        //         pro *= nums[j];
+        //     }
+        //     ans[i] = pro;
+        // }
+    }
+}
