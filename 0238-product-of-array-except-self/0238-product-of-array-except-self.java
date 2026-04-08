b51a2339e4dc3d4ae56aae31747b299c242f6ea6
Md Jawed <jawednaushad@gmail.com>
2026-04-08 20:38:01 +0530

    Time: 1 ms (99.94%), Space: 72.3 MB (20.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,31 +1,47 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        // T.C -> O(N + N)
-        // S.C -> O(N + N)
-
         int n = nums.length;
-        int prefixProduct [] = new int [n];
-        int postfixProduct [] = new int [n];
         int prefix = 1;
-        int postfix = 1;
-
-        // find prefix product of given Array...
+        int[] ans = new int[n];
+        // Calculate Prefix product
         for(int i=0; i<n; i++){
-            prefixProduct[i] = prefix;
+            ans[i] = prefix;
             prefix *= nums[i];
         }
-        // find postfix product of given Array...
+        // Calculate Postfix product and multiply with prefix product to get result.
+        int postfix = 1;
         for(int i=n-1; i>=0; i--){
-            postfixProduct[i] = postfix;
+            ans[i] *= postfix;
             postfix *= nums[i];
         }
+        return ans;
 
-        // Multiply prefix and postfix to get answer.
-        for(int i=0; i<n; i++){
-            nums[i] = prefixProduct[i]*postfixProduct[i];
-        }
-        return nums;
+
+            // Better Solution [TC -> O(n+n+n)=O(n) && SC -> O(n+n)=O(n)]
+
+        // int n = nums.length;
+        // int prefixProduct [] = new int [n];
+        // int postfixProduct [] = new int [n];
+        // int prefix = 1;
+        // int postfix = 1;
+
+        // // find prefix product of given Array...
+        // for(int i=0; i<n; i++){
+        //     prefixProduct[i] = prefix;
+        //     prefix *= nums[i];
+        // }
+        // // find postfix product of given Array...
+        // for(int i=n-1; i>=0; i--){
+        //     postfixProduct[i] = postfix;
+        //     postfix *= nums[i];
+        // }
+
+        // // Multiply prefix and postfix to get answer.
+        // for(int i=0; i<n; i++){
+        //     nums[i] = prefixProduct[i]*postfixProduct[i];
+        // }
+        // return nums;
 
         
 
