2d1269a8558b494103edc411e3aa3a787b1191ab
Md Jawed <jawednaushad@gmail.com>
2026-05-06 16:28:48 +0530

    Time: 4 ms (92.34%), Space: 83.1 MB (27.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,19 +1,41 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         int n = nums.length;
+        // Easy Methods:
+        // 1. Using HashSet
+        // 2. HashTable (Boolean / count element)
+        // 3. Sorting Method
 
         // Floyd's cycle detection(Hare & Tortoise Method)
+
+        int slow = nums[0];
+        int fast = nums[0];
+        // at least one duplicate is there for sure.
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
         
-        // Mark visited index(element)
-        int ind = nums[0];
-        for(int i=0; i<n; i++){
-            if(nums[ind] == -1)
-                return ind;
-            
-            int temp = ind;
-            ind = nums[ind];
-            nums[temp] = -1;
+        // find the head of loop.
+        fast = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return fast;
+
+        
+        // // Mark visited index(element)
+
+        // int ind = nums[0];
+        // for(int i=0; i<n; i++){
+        //     if(nums[ind] == -1)
+        //         return ind;
+            
+        //     int temp = ind;
+        //     ind = nums[ind];
+        //     nums[temp] = -1;
+        // }
+        // return -1;
     }
 }
